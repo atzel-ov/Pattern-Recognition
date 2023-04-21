3e4027934989769fd0b3f5e0f5b7d2fb507254c5
Angelopoulos Dimitris <83914255+atzel-ov@users.noreply.github.com>
2023-04-22 01:09:28 +0300

    Add files via upload

--- SET1/Exercise2/computeAspectRatio.m
+++ SET1/Exercise2/computeAspectRatio.m
@@ -1,8 +1,22 @@
-function aRatio = computeAspectRatio(image)
-    [num_rows, num_cols] = size(image);
+function [aRatio, min_row, min_col, height, width] = computeAspectRatio(image)
+    %[num_rows, num_cols] = size(image);
 
     % Fill your code
-    
+    sum_row = sum(image,2); %array containing the sum of the values of each row of the image
+    sum_col = sum(image,1); %array containing the sum of the values of each col of the image
+
+    % Now we will find the index of the first and last nonzero element of sum_row and sum_col
+
+    min_row = find(sum_row, 1); %index of first nonzero element of the rows
+    max_row = find(sum_row, 1, 'last'); %index of last nonzero element of the rows
+
+    min_col = find(sum_col, 1);
+    max_col = find(sum_col, 1, 'last');
+
+    height = max_row - min_row + 1;
+    width = max_col - min_col + 1;
+
+    aRatio = width/height;
 
 end
 
--- SET1/Exercise2/exercise1_2.m
+++ SET1/Exercise2/exercise1_2.m
@@ -15,16 +15,46 @@ data = load(data_file);
 
 %% Compute Aspect Ratio
 
+%Computing the aspect Ration of each digit and saving it to the
+%corresponding aray 
 
-% Compute the aspect ratios of all images and store the value of the i-th image in aRatios(i)
+aRatio_C1 = zeros(size(train_C1_indices,2),1)';
 
-%    num_C1 =  size(train_C1_images,1);
-%    num_C2 =  size(train_C2_images,1);
-%    
-%    images = num_C1 + num_C2;
+for i = 1:size(train_C1_indices,2)
+    [aRatio_C1(i), min_row_C1(i), min_col_C1(i), height_C1(i), width_C1(i)] = computeAspectRatio(reshape(train_C1_images(i,:,:),28,28));
 
-minAspectRatio = ...
-maxAspectRatio = ...
+end
+
+aRatio_C2 = zeros(size(train_C2_indices,2),1)';
+
+for j = 1:size(train_C2_indices,2)
+    [aRatio_C2(j), min_row_C2(j), min_col_C2(j), height_C2(j), width_C2(j)] = computeAspectRatio(reshape(train_C2_images(j,:,:),28,28));
+end
+
+%Constructing a new array containing every aRtio and then finding the
+%min & max values 
+aRatio = [aRatio_C1 aRatio_C2];
+
+
+minAspectRatio = min(aRatio);
+maxAspectRatio = max(aRatio);
+
+figure 
+imagesc(reshape(train_C1_images(25,:,:),28,28))
+colormap("gray")
+
+rectangle('Position', [min_col_C1(25), min_row_C1(25), width_C1(25), height_C1(25)]', 'EdgeColor', 'r', 'LineWidth', 2)
+
+
+title(sprintf('Digit %d', 25))
+
+
+figure
+imagesc(reshape(train_C2_images(29,:,:),28,28))
+colormap("gray")
+rectangle('Position', [ min_col_C2(29), min_row_C2(29), width_C2(29), height_C2(29)], 'EdgeColor', 'r', 'LineWidth', 2)
+title(sprintf('Digit %d', 29))
+
 
 
 %% Bayesian Classifier
